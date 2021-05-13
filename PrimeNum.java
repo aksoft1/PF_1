9e8c01317cc58a6748d5a843f48fce0ecd164fb7
aksoft1 <84111445+aksoft1@users.noreply.github.com>
2021-05-13 16:19:47 +0900

    [이성훈]NCS_2차평가_소수 구하기_Final.v2
    소수와 합성수를 구별하는 프로그래밍 시험

--- PrimeNum.java
+++ PrimeNum.java
@@ -4,26 +4,29 @@ public class PrimeNum
 {
 	public static void main(String[] args)
 	{
-		int val = 8;
+		int val = 6;
 		int div = 2;
 		boolean PrimeNum = true;
 		
 		for(int i = 0; i < val-2; i++)
 		{
-			if(val % div ==0);
-			PrimeNum = false;
-			
-			break;
+			if(val % div == 0)
+			{
+				PrimeNum = false;
+				break;
+			}
+			div++;
 		}
-
 		if(PrimeNum == true)
 		{
-			
 			System.out.println("입력한 수는 " + val + "입니다");
-			System.out.println("해당 수는 소수입니다" );
+			System.out.println("해당 수는 소수입니다");
 		}
 		else
+		{
 			System.out.println("입력한 수는 " + val + "입니다");
 			System.out.println("해당 수는 합성수입니다");
+		}
 	}
 }
+	
